f6f7108940baa81a5d0371b18d56b583262a34cf
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2024-01-22 17:38:20 +0100

    initialize all folders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <hr />
+        <Route path="/" exact component={Home} />
+      </div>
+    </Router>
   );
 }
 
