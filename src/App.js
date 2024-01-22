9b4afd4a9506c33fc5501c99587e236058098bc7
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2024-01-22 17:49:35 +0100

    add files scss and connect other files between them

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Navbar />
-        <hr />
-        <Route path="/" exact component={Home} />
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Home() {
+    return (
+      <div>
+        <h2>Accueil</h2>
+        {/* Contenu de la page d'accueil */}
+      </div>
+    );
+  }
+  
+  export default Home;
