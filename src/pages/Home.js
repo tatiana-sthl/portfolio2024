0dd6bbd009ab1609fd1871141a6755427e2c96d9
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2024-01-23 17:30:49 +0100

    add variables in scss

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import '../styles/base.scss';
 
 function Home() {
     return (
       <div>
-        <h2>Accueil</h2>
-        {/* Contenu de la page d'accueil */}
+        <h1>Hello World !</h1>
+        <h2>Tatiana Stahli</h2>
+        <h3>Développeuse web</h3>
+        <p>(et bien plus encore)</p>
+        
       </div>
     );
   }
