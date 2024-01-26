2f1d17c2369aa5bffafc39a8b4dfa30a3354c75a
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2024-01-26 18:04:56 +0100

    insert icons in home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import '../styles/pages/_home.scss';
+import Book from '../icons/book-icon.svg';
 
 function Home() {
   return (
-    <div>
-      <h1>HELLO WORLD !</h1>
-      <h2>Tatiana Stahli</h2>
-      <h3>Développeuse web</h3>
-      <p>(et bien plus encore)</p>
-    </div>
+    <>
+      <div>
+        <h1>HELLO WORLD !</h1>
+        <h2>Tatiana Stahli</h2>
+        <h3>Développeuse web</h3>
+        <h4>(et bien plus encore)</h4>
+      </div>
+
+      <div>
+        <div>
+          <h3>PROJETS</h3>
+          <div>
+            <img src={Book} alt='book-icon'/>
+          </div>
+        </div>
+
+      </div>
+    </>
   );
 }
 
