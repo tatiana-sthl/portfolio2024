a97148edd7b37b06667d347dc3f68b288a53a241
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2024-02-21 17:21:04 +0100

    add link between pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
+import Cv from './pages/Cv';
+import Projects from './pages/Projects';
+import Stack from './pages/Stack';
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
@@ -17,6 +21,9 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/cv" element={<Cv />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/stack" element={<Stack />} />
           </Routes>
         </div>
       </div>
