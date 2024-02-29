cef48e4c8c47bc53c8e691fc58d056a25398795c
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2024-02-29 14:41:18 +0100

    add formspree for contact

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,11 +1,59 @@
-import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 
 
-function ContactForm() {
-    return (
-      <>
-      </>
-    )
-}
+const ContactForm = () => {
+
+  const [state, handleSubmit] = useForm("xjvnrjgy");
+  if (state.succeeded) {
+      return <p>Je vous recontacte très bientôt !</p>;
+  };
+  
+  return (
+    <>
+    <div>  
+      <form className='contactForm' onSubmit={handleSubmit}>
+        <div className='mailInfos'>
+          <div className='formGroup'>
+            <label className='label' htmlFor="email">Email :</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className='input'
+              placeholder='exemple@gmail.com'
+            />
+            <ValidationError 
+              prefix="Email" 
+              field="email"
+              errors={state.errors}
+            />
+          </div>
+        </div>
+              
+        <div className='formGroupTextarea'>
+          <label className='label' htmlFor="message">Message :</label>
+          <textarea
+            id="message"
+            name="message"
+            className='textarea'
+            placeholder=
+            {`Bonjour vous deux, Nous serons trois pour le vin d'honneur/repas. Il y aura l'empereur, sa femme et le petit prince de 5 ans. Au plaisir de vous voir !`}
+          />
+          <ValidationError 
+            prefix="Message" 
+            field="message"
+            errors={state.errors}
+          />
+        </div>
+        <div className='formButton'>
+          <button className='button' type="submit" disabled={state.submitting}>Envoyer</button>
+        </div>
+        
+      </form>
+    </div>
+
+    </>
+  );
+};
 
 export default ContactForm;
