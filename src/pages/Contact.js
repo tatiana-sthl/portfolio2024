55896008b9a78495c0675b0fab35db028d4338df
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2024-03-16 17:30:21 +0100

    finish contact responsive

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,9 +2,6 @@ import React from 'react';
 import '../styles/pages/_contact.scss';
 import ContactForm from '../components/ContactForm';
 
-/* TODO : 
-    - faire le responsive
-*/
 
 function Contact() {
     return (
@@ -17,9 +14,25 @@ function Contact() {
             </div>
             <div className='contactRS'>
               <h4>Vous pouvez aussi me contacter par ici :</h4>
-              <a href='https://github.com/tatiana-sthl' target="_blank" rel="noreferrer">Github</a><p>·</p>
-              <a href='https://www.linkedin.com/in/tatiana-stahli/' target="_blank" rel="noreferrer">Linkedin</a><p>·</p>
-              <a href='https://twitter.com/ThathiDev' target="_blank" rel="noreferrer">Twitter</a>
+              <div className='RSLink'>
+                <a href='https://github.com/tatiana-sthl' target="_blank" rel="noreferrer">Github</a><p>·</p>
+                <a href='https://www.linkedin.com/in/tatiana-stahli/' target="_blank" rel="noreferrer">Linkedin</a><p>·</p>
+                <a href='https://twitter.com/ThathiDev' target="_blank" rel="noreferrer">Twitter</a>
+              </div>
+              <div className='RSLinkMobile'>
+                <ul>
+                  <li className='RSLinkMobileLi'>
+                    <a href='https://github.com/tatiana-sthl' target="_blank" rel="noreferrer">Github</a>
+                  </li>
+                  <li className='RSLinkMobileLi'>
+                    <a href='https://twitter.com/ThathiDev' target="_blank" rel="noreferrer">Twitter</a>
+                  </li>
+                  <li className='RSLinkMobileLi'>
+                    <a href='https://www.linkedin.com/in/tatiana-stahli/' target="_blank" rel="noreferrer">Linkedin</a>
+                  </li>
+                </ul>
+              </div>
+              
             </div>
       </>
     )
