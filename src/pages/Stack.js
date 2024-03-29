ab07567b969184c3acae41053da35be84137c18a
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2024-03-29 18:06:24 +0100

    start working on stack page

--- src/pages/Stack.js
+++ src/pages/Stack.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import '../styles/pages/_stack.scss';
 
 function Stack() {
     return (
       <>
+      <div className='stackTitle'>
+        <h1>Stack</h1>
+      </div>
       </>
     )
 }
