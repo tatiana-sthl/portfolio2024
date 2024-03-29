d90e877ad9285107278c01a9d125838d499a0873
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2024-03-29 18:30:19 +0100

    add name about stack

--- src/pages/Stack.js
+++ src/pages/Stack.js
@@ -6,6 +6,20 @@ function Stack() {
       <>
       <div className='stackTitle'>
         <h1>Stack</h1>
+        <div className='divStack'>
+          <span>HTML</span>
+          <span>CSS</span>
+          <span>JS</span>
+          <span>React</span>
+          <span>PHP</span>
+          <span>MongoDB</span>
+          <br></br>
+          <span>Bootstrap</span>
+          <span>VSC</span>
+          <span>Figma</span>
+          <span>Postman</span>
+          <span>Wordpress</span>
+        </div>
       </div>
       </>
     )
