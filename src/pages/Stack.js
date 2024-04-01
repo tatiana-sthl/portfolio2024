8f61f3f7a4489a53f50c03fb65e3af86925dc41d
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2024-04-01 10:24:26 +0200

    first try about page stack

--- src/pages/Stack.js
+++ src/pages/Stack.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import '../styles/pages/_stack.scss';
+import '../styles/components/_navbar.scss';
+
+import HtmlIcon from '../icons/icons8-html-5.svg';
+
 
 
 
@@ -8,21 +12,27 @@ function Stack() {
       <>
       <div className='stackTitle'>
         <h1>Stack</h1>
-        <div className='divStack'>
-          <span>HTML</span>
-          <span>CSS</span>
-          <span>JS</span>
-          <span>React</span>
-          <span>PHP</span>
-          <span>MongoDB</span>
-          <br></br>
-          <span>Bootstrap</span>
-          <span>VSC</span>
-          <span>Figma</span>
-          <span>Postman</span>
-          <span>Wordpress</span>
-        </div>
       </div>
+      <div className='divStack'>
+        <div className='iconContainer'>
+          <img src={HtmlIcon} alt="HTML Icon" />
+          <span>HTML</span>
+        </div>
+        
+        
+        <span>CSS</span>
+        <span>JS</span>
+        <span>React</span>
+        <span>PHP</span>
+        <span>MongoDB</span>
+        <br></br>
+        <span>Bootstrap</span>
+        <span>VSC</span>
+        <span>Figma</span>
+        <span>Postman</span>
+        <span>Wordpress</span>
+
+    </div>
       </>
     )
 }
