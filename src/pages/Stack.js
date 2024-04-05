2353e9ee2614af2c61000089d1804079b4dfcd9d
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2024-04-05 19:18:51 +0200

    add name in import stack

--- src/pages/Stack.js
+++ src/pages/Stack.js
@@ -3,8 +3,16 @@ import '../styles/pages/_stack.scss';
 import '../styles/components/_navbar.scss';
 
 import HtmlIcon from '../icons/icons8-html-5.svg';
-
-
+import CssIcon from '../icons/icons8-html-5.svg';
+import JSIcon from '../icons/icons8-html-5.svg';
+import ReactIcon from '../icons/icons8-html-5.svg';
+import PhpIcon from '../icons/icons8-html-5.svg';
+import MongoDBIcon from '../icons/icons8-html-5.svg';
+import BootstrapIcon from '../icons/icons8-html-5.svg';
+import VscIcon from '../icons/icons8-html-5.svg';
+import FigmaIcon from '../icons/icons8-html-5.svg';
+import PostmanIcon from '../icons/icons8-html-5.svg';
+import WordpressIcon from '../icons/icons8-html-5.svg';
 
 
 function Stack() {
@@ -17,6 +25,10 @@ function Stack() {
         <div className='iconContainer'>
           <img className="imgIcon" src={HtmlIcon} alt="HTML Icon" />
           <span className='iconText'>HTML</span>
+        </div>
+        <div className='iconContainer'>
+          <img className="imgIcon" src={HtmlIcon} alt="HTML Icon" />
+          <span className='iconText'>CSS</span>
         </div>
         
         
