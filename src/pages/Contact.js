c4e835a026ed6a9326c866436fb2cd9bf2a85a2b
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2024-05-20 10:44:56 +0200

    start working on cv page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,33 +7,33 @@ function Contact() {
     return (
       <>
       <div className='contactTitle'>
-                <h1>Contact</h1>
-            </div>
-            <div>
-              <ContactForm />
-            </div>
-            <div className='contactRS'>
-              <h4>Vous pouvez aussi me contacter par ici :</h4>
-              <div className='RSLink'>
-                <a href='https://github.com/tatiana-sthl' target="_blank" rel="noreferrer">Github</a><p>·</p>
-                <a href='https://www.linkedin.com/in/tatiana-stahli/' target="_blank" rel="noreferrer">Linkedin</a><p>·</p>
-                <a href='https://twitter.com/ThathiDev' target="_blank" rel="noreferrer">Twitter</a>
-              </div>
-              <div className='RSLinkMobile'>
-                <ul>
-                  <li className='RSLinkMobileLi'>
-                    <a href='https://github.com/tatiana-sthl' target="_blank" rel="noreferrer">Github</a>
-                  </li>
-                  <li className='RSLinkMobileLi'>
-                    <a href='https://twitter.com/ThathiDev' target="_blank" rel="noreferrer">Twitter</a>
-                  </li>
-                  <li className='RSLinkMobileLi'>
-                    <a href='https://www.linkedin.com/in/tatiana-stahli/' target="_blank" rel="noreferrer">Linkedin</a>
-                  </li>
-                </ul>
-              </div>
-              
-            </div>
+          <h1>Contact</h1>
+      </div>
+      <div>
+        <ContactForm />
+      </div>
+      <div className='contactRS'>
+        <h4>Vous pouvez aussi me contacter par ici :</h4>
+        <div className='RSLink'>
+          <a href='https://github.com/tatiana-sthl' target="_blank" rel="noreferrer">Github</a><p>·</p>
+          <a href='https://www.linkedin.com/in/tatiana-stahli/' target="_blank" rel="noreferrer">Linkedin</a><p>·</p>
+          <a href='https://twitter.com/ThathiDev' target="_blank" rel="noreferrer">Twitter</a>
+        </div>
+        <div className='RSLinkMobile'>
+          <ul>
+            <li className='RSLinkMobileLi'>
+              <a href='https://github.com/tatiana-sthl' target="_blank" rel="noreferrer">Github</a>
+            </li>
+            <li className='RSLinkMobileLi'>
+              <a href='https://twitter.com/ThathiDev' target="_blank" rel="noreferrer">Twitter</a>
+            </li>
+            <li className='RSLinkMobileLi'>
+              <a href='https://www.linkedin.com/in/tatiana-stahli/' target="_blank" rel="noreferrer">Linkedin</a>
+            </li>
+          </ul>
+        </div>
+        
+      </div>
       </>
     )
 }
--- src/pages/Cv.js
+++ src/pages/Cv.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import '../styles/pages/_cv.scss';
 
 function Cv() {
     return (
       <>
+      <div>
+          <h1>Parcours</h1>
+      </div>
       </>
     )
 }
